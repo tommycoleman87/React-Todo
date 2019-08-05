02f5e6b2fa3d95361ab793d463b0e35b2bbc1a79
Tommy Coleman <tommiilee@gmail.com>
2019-08-05 18:37:50 -0400

    did stretch to use local storage to persist data

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
 import React from 'react';
 import TodoList from '../src/components/TodoComponents/TodoList';
 import TodoForm from '../src/components/TodoComponents/TodoForm';
+
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+ 
   constructor() {
     super();
-    this.state = {
-      toDos: [],
-      task: '',
-      searchFilter: '',
-    }
+    
+    this.state = {toDos: [], task: '', searchFilter: ''}
   }
 
-
+  componentDidMount() {
+    const newState = JSON.parse(window.localStorage.getItem('state'))
+    this.setState(newState)
+    console.log(window.localStorage)
+  }
 
   submitTaskHandler = (e) => {
-    e.preventDefault();
-    const newTask = {
-      task: this.state.task,
-      id: Date.now(),
-      completed: false
-    }
-    this.setState({
-      toDos: [...this.state.toDos, newTask],
-      task: '',
-      searchFilter: '',
-    })
+    const newTask = {task: this.state.task, id: Date.now(), completed: false}
+    window.localStorage.clear();
+    window.localStorage.setItem('state', JSON.stringify({toDos: [...this.state.toDos, newTask], task: '', searchFilter: ''}))
+    console.log(newTask)
+    console.log(this.state)
+  
   }
 
   inputChangeHandler = (e) => {
@@ -67,13 +66,13 @@ class App extends React.Component {
       searchFilter: e.target.value,
     })
   }
-
+ 
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
         <TodoForm addTask={this.submitTaskHandler} inputHandler={this.inputChangeHandler} task={this.state.task}/>
-        <TodoList toDos={this.state.toDos} toggleTask={this.toggleTask} searchFilter={this.state.searchFilter} />
+        <TodoList toDos={this.state.toDos} toggleTask={this.toggleTask} searchFilter={this.state.searchFilter} /> 
         <button onClick={this.clearCompleted}>Clear Completed</button>
         <input onChange={this.searchFilterHandler} placeholder='Search Tasks' />
       </div>
